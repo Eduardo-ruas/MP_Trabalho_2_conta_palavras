26220dfdfb5b8c795011ed951b2c328696b32e37
eduardoruas <eduardobritoruas@gmail.com>
2025-01-08 11:36:49 -0300

    Arquivo testa_conta_palavra.cpp em desenvolvimento

--- testa_conta_palavras.cpp
+++ testa_conta_palavras.cpp
@@ -1,14 +1,17 @@
 #define CATCH_CONFIG_MAIN  // Isso diz ao Catch2 para fornecer uma função main()
 #include "catch.hpp"
+#include "conta_palavras.hpp"
+#include <fstream>
 
-TEST_CASE("Conta palavras de um arquivo") {
-    auto resultado = conta_palavras_arquivo("texto.txt");
+TEST_CASE("Conta palavras em uma string simples") {
+    std::string texto = "Este texto é o texto que será utilizado";
+    auto resultado = conta_palavras(texto);
 
+    REQUIRE(resultado["Este"] == 1);
+    REQUIRE(resultado["texto"] == 2);
     REQUIRE(resultado["é"] == 1);
-    REQUIRE(resultado["este"] == 1);
     REQUIRE(resultado["o"] == 1);
     REQUIRE(resultado["que"] == 1);
     REQUIRE(resultado["será"] == 1);
-    REQUIRE(resultado["texto"] == 2);
     REQUIRE(resultado["utilizado"] == 1);
 }
